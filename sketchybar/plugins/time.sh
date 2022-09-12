ea177a7550a20b5b493c4ff37ce26ca65e29c802
itaysharir <87126382+itaysharir@users.noreply.github.com>
2022-09-12 22:02:27 +0300

    Deleting sketchybar diectory
    To upload a newer verison!

--- sketchybar/plugins/time.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-sketchybar --set $NAME icon="$(date '%a %b %d, %Y,')" label="$(date '+%H:%M')"
--- sketchybar/plugins/window_title.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-update() {
-  sketchybar --set $NAME label="$INFO"
-}
-
-TITLE=$(yabai -m query --windows --window | jq -r '.title')
-sketchybar --set $NAME label="$TITLE"
-
