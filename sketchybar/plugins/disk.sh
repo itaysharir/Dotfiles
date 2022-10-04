62d4eede35b21a23e7d991a51a9f73be29db2632
itaysharir <87126382+itaysharir@users.noreply.github.com>
2022-10-04 14:35:20 +0300

    Small change to widget that shows disk space in bar

--- sketchybar/plugins/disk.sh
+++ sketchybar/plugins/disk.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env sh
 
-TOPPROC=$(TARGET_PATH="."
-df -h "$TARGET_PATH"  | awk 'NR==2{print $4}'
-)
+TOPPROC=$(df -h "/" | awk 'NR==2{print $4}' | cut -c 1-4)
 sketchybar --set $NAME icon="" label="ssd: $TOPPROC free"
