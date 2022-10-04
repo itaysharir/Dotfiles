0a3fad023b788ef89789f564f6c13f2229e6d9cc
itaysharir <87126382+itaysharir@users.noreply.github.com>
2022-10-04 12:34:12 +0300

    Delete sketrchybar directory (To upload a new one)

--- sketchybar/plugins/disk.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-TOPPROC=$(TARGET_PATH="."
-df -h "$TARGET_PATH"  | awk 'NR==2{print $4}'
-)
-sketchybar --set $NAME icon="" label="ssd: $TOPPROC free"
--- sketchybar/plugins/uptime.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-status=$(uptime | cut -d',' -f 1 | cut -d 'p' -f 2-)
-uptime=$(echo $status)
-
-sketchybar --set $NAME icon="" label="uptime: $uptime"
