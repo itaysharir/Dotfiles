88d51d5f5edd1e2e33dd34bf93a5abcd8679c620
itaysharir <87126382+itaysharir@users.noreply.github.com>
2022-10-08 21:49:12 +0300

    Update systray

--- sketchybar/items/system.sh
+++ sketchybar/items/system.sh
@@ -1,48 +1,31 @@
 #!/usr/bin/env sh
 
-HAS_BATTERY=$(if [ "$(pmset -g batt | grep "Battery")" = "" ]; then echo "false"; else echo "true"; fi)
-SVIM_POPUP_SCRIPT="sketchybar --set system.svim popup.drawing=off"
-
-sketchybar --add       item               system.svim right                                 \
-           --set       system.svim        popup.align=left                              \
-                                          icon=$VIM                                     \
-                                          icon.font="Hack Nerd Font Mono:Bold:26.0"     \
-                                          label.font="Hack Nerd Font Mono:Bold:12.0"    \
-                                          icon.color=$ICON_COLOR                        \
-                                          script="$SVIM_POPUP_SCRIPT"                   \
-                                          background.padding_right=-11                    \
-           --subscribe system.svim        front_app_switched window_focus               \
+sketchybar --add       item               discord right                                       \
+           --set       discord            background.image=/Users/itaysharir/.config/sketchybar/discord-logo.png \
+                                          background.image.scale=0.015                            \
+                                          background.drawing=on                                 \
+                                          background.padding_right=4                            \
                                                                                         \
-           --add       item               svim.cmdline popup.system.svim                \
-           --set       svim.cmdline       icon="Command: "                              \
-
 sketchybar --add       alias              "Control Center,WiFi" right                       \
            --set    "Control Center,WiFi" update_freq=1                                 \
                                           icon.drawing=off                              \
                                           label.drawing=off                             \
-                                          background.padding_left=-9                    \
+                                          background.padding_left=-11                    \
                                           background.padding_right=-12                   \
+
+sketchybar --add       item               spotify right                                       \
+           --set       spotify            background.image=/Users/itaysharir/.config/sketchybar/spotify-logo.png \
+                                          background.image.scale=0.007                            \
+                                          background.drawing=on                                     \
 
 sketchybar --add       alias              "Control Center,Sound" right                       \
            --set    "Control Center,Sound" update_freq=1                                 \
                                           icon.drawing=off                              \
-                                          label.drawing=off                             \
-                                          background.padding_left=-8                    \
-                                          background.padding_right=-7                   \
+                                          label.font="Font Awesome 6 Brands:Regular:12.0"           \
+                                          background.padding_right=-17                           \
+                                          background.padding_left=-10                           \
 
-sketchybar --add       alias              "SystemUIServer,AppleTimeMachineExtra" right                       \
-           --set    "SystemUIServer,AppleTimeMachineExtra"   update_freq=1                                 \
-                                          icon.drawing=off                              \
-                                          label.drawing=off                             \
-                                          background.padding_left=-19                    \
-                                          background.padding_right=-9                   \
-
-sketchybar --add       alias              "Control Center,BentoBox" right                       \
-           --set    "Control Center,BentoBox"   update_freq=1                                 \
-                                          icon.drawing=off                              \
-                                          label.drawing=off                             \
-                                          background.padding_left=-9                    \
-                                          background.padding_right=4                   \
-
-           --add       bracket            system                                        \
-                                          system.svim                                   \
+sketchybar --add       item               obs right                                       \
+           --set       obs                background.image=/Users/itaysharir/.config/sketchybar/obs-logo.png \
+                                          background.image.scale=0.028                            \
+                                          background.drawing=on                                 \
