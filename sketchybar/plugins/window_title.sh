a4f45c02731edfbf261504a2af4c1d28f643a540
itaysharir <87126382+itaysharir@users.noreply.github.com>
2022-11-14 15:10:07 +0200

    Delete sketchybar directory (To upload a new one)

--- sketchybar/plugins/window_title.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-update() {
-  sketchybar --set $NAME label="$INFO"
-}
-
-TITLE=$(yabai -m query --windows --window | jq -r '.title' | sed 's/\(.\{80\}\).*/\1.../')
-sketchybar --set $NAME label="$TITLE"
