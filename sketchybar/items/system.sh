f8909c4217c5c21bbc22ecdb5d3b15e16fd2fa5e
Itay Sharir <itaysharir61@gmail.com>
2022-11-23 20:36:03 +0200

    Add back systray to sketchybar

--- sketchybar/items/system.sh
+++ sketchybar/items/system.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env sh
 
-# sketchybar --add       item               discord right                                       \
-#            --set       discord            background.image=/Users/itaysharir/.config/sketchybar/assets/discord-logo.png \
-#                                           background.image.scale=0.015                            \
-#                                           background.drawing=on                                 \
-#                                           background.padding_right=4                            \
+sketchybar --add       item               discord right                                       \
+           --set       discord            background.image=/Users/itaysharir/.config/sketchybar/assets/discord-logo.png \
+                                          background.image.scale=0.015                            \
+                                          background.drawing=on                                 \
+                                          background.padding_right=4                            \
 
-sketchybar --add       alias              "Cardhop,Item-0" right                       \
-           --set    "Cardhop,Item-0" update_freq=1                                 \
-                                          icon.drawing=off                              \
-                                          label.drawing=off                             \
-                                          background.padding_left=-10                    \
-                                          background.padding_right=-11                            \
+# sketchybar --add       alias              "Cardhop,Item-0" right                       \
+#            --set    "Cardhop,Item-0" update_freq=1                                 \
+#                                           icon.drawing=off                              \
+#                                           label.drawing=off                             \
+#                                           background.padding_left=-10                    \
+#                                           background.padding_right=-11                            \
 
 
 sketchybar --add       alias              "Control Center,WiFi" right                       \
