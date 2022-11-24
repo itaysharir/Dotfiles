3420134ff0ac9a1f1d7341aecd273a01645b8a6d
Itay Sharir <itaysharir61@gmail.com>
2022-11-24 17:07:37 +0200

    Add more systray icons

--- sketchybar/items/system.sh
+++ sketchybar/items/system.sh
@@ -5,6 +5,7 @@ sketchybar --add       item               discord right                         
                                           background.image.scale=0.015                            \
                                           background.drawing=on                                 \
                                           background.padding_right=4                            \
+                                          background.padding_left=-2                            \
 
 # sketchybar --add       alias              "Cardhop,Item-0" right                       \
 #            --set    "Cardhop,Item-0" update_freq=1                                 \
@@ -13,7 +14,6 @@ sketchybar --add       item               discord right                         
 #                                           background.padding_left=-10                    \
 #                                           background.padding_right=-11                            \
 
-
 sketchybar --add       alias              "Control Center,WiFi" right                       \
            --set    "Control Center,WiFi" update_freq=1                                 \
                                           icon.drawing=off                              \
@@ -21,24 +21,12 @@ sketchybar --add       alias              "Control Center,WiFi" right           
                                           background.padding_left=-10                    \
                                           background.padding_right=-8                            \
 
-# sketchybar --add       alias              "Raycast,raycastIcon" right                       \
-#            --set    "Control Center,WiFi Raycast,raycastIcon" update_freq=1                                 \
-#                                           icon.drawing=off                              \
-#                                           label.drawing=off                             \
-#                                           background.padding_left=-10                    \
-#                                           background.padding_right=-8                            \
-
-# sketchybar --add       item               spotify right                                       \
-#            --set       spotify            background.image=/Users/itaysharir/.config/sketchybar/assets/spotify-logo.png \
-#                                           background.image.scale=0.007                            \
-#                                           background.drawing=on                                     \
-
-# sketchybar --add       alias              "iStat Menus Status,com.bjango.istatmenus.network" right                       \
-#            --set    "iStat Menus Status,com.bjango.istatmenus.network"  update_freq=1                                 \
-#                                           icon.drawing=off                              \
-#                                           label.drawing=off                             \
-#                                           background.padding_left=-4                    \
-#                                           background.padding_right=-8                   \
+sketchybar --add       item               net right                                       \
+           --set       net                background.image=/Users/itaysharir/.config/sketchybar/assets/network.png \
+                                          background.image.scale=0.03                            \
+                                          background.drawing=on                                 \
+                                          background.padding_right=-6                            \
+                                          background.padding_left=-1                    \
 
 sketchybar --add       alias              "Control Center,Sound" right                       \
            --set    "Control Center,Sound" update_freq=1                                 \
@@ -47,7 +35,7 @@ sketchybar --add       alias              "Control Center,Sound" right          
                                           background.padding_right=-14                           \
                                           background.padding_left=-7                           \
 
-# sketchybar --add       item               obs right                                       \
-#            --set       obs                background.image=/Users/itaysharir/.config/sketchybar/assets/obs-logo.png \
-#                                           background.image.scale=0.028                            \
-#                                           background.drawing=on                                 \
+sketchybar --add       item               obs right                                       \
+           --set       obs                background.image=/Users/itaysharir/.config/sketchybar/assets/obs-logo.png \
+                                          background.image.scale=0.028                            \
+                                          background.drawing=on                                 \
